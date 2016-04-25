f9ecc54f75d6f8dd8d813cf9fd837e9b3ebc7099
David Falcón Naranjo <david_el_125@hotmail.com>
2016-04-25 18:14:20 +0000

    Search function works

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,6 +5,7 @@ import com.mashape.unirest.http.Unirest;
 import com.mashape.unirest.http.exceptions.UnirestException;
 import org.json.JSONObject;
 import spark.ModelAndView;
+import spark.Request;
 import spark.template.jade.JadeTemplateEngine;
 import com.google.common.io.ByteStreams;
 import com.google.common.io.Closeables;
@@ -13,7 +14,11 @@ import com.google.common.net.MediaType;
 import java.io.*;
 import java.util.HashMap;
 import java.util.Iterator;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
 
 import static spark.Spark.*;
 
@@ -23,6 +28,8 @@ public class App {
         threadPool(8);
 
         get("/", (req, res) -> new ModelAndView(content(),"App.jade"), new JadeTemplateEngine());
+
+        post("/search", (req, res) -> new ModelAndView(queryContent(req), "searchResult.jade"), new JadeTemplateEngine());
 
         get("/favicon.ico", (req, res) -> {
             try {
@@ -49,22 +56,25 @@ public class App {
         });
     }
 
-    private static Map<String, String> content() {
+    private static Map<String, String> queryContent(Request req) {
         Map<String, String> map = new HashMap<>();
         map.put("title", "Hearthstone App");
         try {
-            String search = "Frog";
+            String search = req.queryParams("search");
             HttpResponse<JsonNode> response = Unirest.get("https://omgvamp-hearthstone-v1.p.mashape.com/cards/search/"+search)
                     .header("X-Mashape-Key", "fmzbntRqkFmshCSZBJk2203AN8Ybp1KGrcijsnaHeDRpsXG9if")
                     .asJson();
             System.out.println(response.getBody());
-
-
-
-
+            map.put("img", ((JSONObject) response.getBody().getArray().get(0)).get("img").toString());
         } catch (UnirestException e) {
             e.printStackTrace();
         }
+        return map;
+    }
+
+    private static Map<String, String> content() {
+        Map<String, String> map = new HashMap<>();
+        map.put("title", "Hearthstone App");
         return map;
     }
 }
